80e8607fbaea3ee36199b030dacb52f21a8aba7d
Himanshu Tatawat <himanshutatawat9@gmail.com>
2024-05-12 21:11:47 +0530

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,26 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import ApexCharts from 'react-apexcharts';
-import styles from "./Home.module.css";
 import { CircularProgress } from '@mui/material';
-
 import bg from "./bg.png";
-import LoggedInUsers from "../LoggedInUsers/LoggedInUsers"
+import LoggedInUsers from "../LoggedInUsers/LoggedInUsers";
 import { useAuth } from '../Context/AuthContext';
+import styles from "./Home.module.css";
 
 function Home(props) {
-  const [series, setSeries] = useState([{
-    data: generateInitialData()
-  }]);
+  const [series, setSeries] = useState([{ data: generateInitialData() }]);
   const { currentUser } = useAuth();
   const [isLoading, setIsLoading] = useState(true);
 
-  console.log(currentUser,"currr ")
   const options = {
     chart: {
       type: 'candlestick',
-      height: 500, // Set chart height
-      width: '100%' // Set chart width
+      height: 500,
+      width: '100%'
     },
     title: {
       text: 'Stock Price Movement',
@@ -31,7 +27,7 @@ function Home(props) {
     },
     tooltip: {
       enabled: true,
-      theme: 'dark', // You can use 'dark' or 'light' based on your preference or set custom styles
+      theme: 'dark',
       style: {
         fontSize: '12px',
         fontFamily: undefined
@@ -45,7 +41,6 @@ function Home(props) {
           return val.toFixed(2);
         }
       },
-      // Custom tooltip style
       fillSeriesColor: false,
       marker: {
         show: false,
@@ -63,23 +58,21 @@ function Home(props) {
       }
     }
   };
-  
 
   useEffect(() => {
     const interval = setInterval(() => {
       const newData = [...series[0].data, ...generateNewData(series[0].data)];
       setSeries([{ data: newData }]);
       setIsLoading(false);
-    }, 5000);  
+    }, 5000);
     return () => clearInterval(interval);
   }, [series]);
 
   return (
     <div className={styles.container}>
-      
       <div className={styles.navbar}>
         <div className={styles.logo}>
-          <img src={bg} style={{ width: "90px", height: "50px" }} />
+          <img src={bg} alt="Logo" className={styles.logoImage} />
         </div>
         <div className={styles.navLinks}>
           {currentUser ? (
@@ -92,51 +85,55 @@ function Home(props) {
           )}
         </div>
       </div>
-     
-      <h1 className={`${styles.heading} `}>Trade Crypto Perpetual Futures in <span style={{color:"blue"}}> INR </span></h1>
+      <h1 className={`${styles.heading} `}>Trade Crypto Perpetual Futures in <span style={{ color: "blue" }}> INR </span></h1>
       <p className={`${styles.message} ${styles.neonEffect}`}>{currentUser ? `Welcome back, ${currentUser?.displayName}!` : "Please login to view real-time updates."}</p>
       {currentUser ? (
-        isLoading?(<div style={{ textAlign: 'center', padding: '20px' }}>
-        <CircularProgress />
-        <p>Loading data...</p>
-      </div>):(<LoggedInUsers/>)
-
-) :(<div>
-  <ApexCharts options={options} series={series} type="candlestick" height={410} width={500}/>
-</div>)}
+        isLoading ? (
+          <div className={styles.loadingContainer}>
+            <CircularProgress />
+            <p>Loading data...</p>
+          </div>
+        ) : (
+          <LoggedInUsers />
+        )
+      ) : (
+        <div className={styles.chartContainer}>
+          <ApexCharts options={options} series={series} type="candlestick" height={410} width={500} />
+        </div>
+      )}
     </div>
   );
 }
 
 function generateInitialData() {
   const data = [];
-  let time = new Date(); // Start from today
-  time.setHours(0, 0, 0, 0); // Normalize the time part to start of day
+  let time = new Date();
+  time.setHours(0, 0, 0, 0);
 
-  for (let i = 0; i < 60; i++) { // Change from 10 to 30 for one month data
+  for (let i = 0; i < 60; i++) {
     data.push({
       x: new Date(time),
       y: [
-        Math.floor(Math.random() * 100) + 50, // Open
-        Math.floor(Math.random() * 100) + 60, // High
-        Math.floor(Math.random() * 100) + 40, // Low
-        Math.floor(Math.random() * 100) + 55  // Close
+        Math.floor(Math.random() * 100) + 50,
+        Math.floor(Math.random() * 100) + 60,
+        Math.floor(Math.random() * 100) + 40,
+        Math.floor(Math.random() * 100) + 55
       ]
     });
-    time.setDate(time.getDate() - 1); // Move back one day
+    time.setDate(time.getDate() - 1);
   }
-  return data.reverse(); // Reverse to start the oldest to the newest
+  return data.reverse();
 }
 
 function generateNewData(data) {
   const lastData = data[data.length - 1];
-  const newTime = new Date(lastData.x).getTime() + 86400000; // plus one day
+  const newTime = new Date(lastData.x).getTime() + 86400000;
   return [{
     x: new Date(newTime),
     y: [
-      Math.floor(Math.random() * 100) + 50, 
-      Math.floor(Math.random() * 100) + 60, 
-      Math.floor(Math.random() * 100) + 40, 
+      Math.floor(Math.random() * 100) + 50,
+      Math.floor(Math.random() * 100) + 60,
+      Math.floor(Math.random() * 100) + 40,
       Math.floor(Math.random() * 100) + 55
     ]
   }];
